9088bf98c4cb81a15b0860ff31bc0db01ca57ec2
johnkauan <johnkauanpassos123@gmail.com>
2026-05-11 16:13:20 -0300

    feat: initialize spring boot projet

---
+++ pediuber/src/main/java/com/pediuber/pediuber/PediuberApplication.java
@@ -0,0 +1,13 @@
+package com.pediuber.pediuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PediuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PediuberApplication.class, args);
+	}
+
+}
---
+++ pediuber/src/test/java/com/pediuber/pediuber/PediuberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pediuber.pediuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PediuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

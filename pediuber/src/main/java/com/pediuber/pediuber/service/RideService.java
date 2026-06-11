e4d1767f6bf8351f881d71a59bbfb4ebe4226f06
johnkauan <johnkauanpassos123@gmail.com>
2026-06-11 20:44:38 -0300

    implementando testes de filas e fluxo

--- pediuber/src/main/java/com/pediuber/pediuber/service/RideService.java
+++ pediuber/src/main/java/com/pediuber/pediuber/service/RideService.java
@@ -1,7 +1,5 @@
 package com.pediuber.pediuber.service;
 
-
-import com.pediuber.pediuber.dto.CreateRideRequest;
 import com.pediuber.pediuber.entity.Driver;
 import com.pediuber.pediuber.entity.Passenger;
 import com.pediuber.pediuber.entity.Ride;
@@ -13,6 +11,9 @@ import com.pediuber.pediuber.repository.PassengerRepository;
 import com.pediuber.pediuber.repository.RideRepository;
 import org.springframework.stereotype.Service;
 import com.pediuber.pediuber.logging.LoggingService;
+import com.pediuber.pediuber.dto.RideQueueMessage;
+import com.pediuber.pediuber.policy.OverflowPolicyService;
+import com.pediuber.pediuber.rabbitmq.RideProducer;
 
 import java.time.LocalDateTime;
 
@@ -24,19 +25,25 @@ public class RideService {
     private final PassengerRepository passengerRepository;
     private final PendingRidePool pendingRidePool;
     private final LoggingService loggingService;
+    private final OverflowPolicyService overflowPolicyService;
+    private final RideProducer rideProducer;
 
     public RideService(
             RideRepository rideRepository,
             DriverRepository driverRepository,
             PassengerRepository passengerRepository,
             PendingRidePool pendingRidePool,
-            LoggingService loggingService
+            LoggingService loggingService,
+            OverflowPolicyService overflowPolicyService,
+            RideProducer rideProducer
     ) {
         this.rideRepository = rideRepository;
         this.driverRepository = driverRepository;
         this.passengerRepository = passengerRepository;
         this.pendingRidePool = pendingRidePool;
         this.loggingService = loggingService;
+        this.overflowPolicyService = overflowPolicyService;
+        this.rideProducer = rideProducer;
     }
 
     public Ride createRide(Long passengerId, Ride ride) {
@@ -61,6 +68,35 @@ public class RideService {
                         null
                 )
         );
+
+        if (overflowPolicyService.isOverloaded()) {
+
+            RideQueueMessage message = new RideQueueMessage(
+                    savedRide.getId(),
+                    savedRide.getOrigin(),
+                    savedRide.getDestination(),
+                    passenger.getId(),
+                    savedRide.getStatus().name(),
+                    LocalDateTime.now().toString(),
+                    0
+            );
+
+            rideProducer.sendRideToOutputQueue(message);
+
+            loggingService.warn(
+                    new LogEvent(
+                            LocalDateTime.now().toString(),
+                            "RIDE_SENT_TO_OUTPUT_QUEUE",
+                            savedRide.getId(),
+                            "PediUber",
+                            savedRide.getStatus().name(),
+                            savedRide.getStatus().name(),
+                            null
+                    )
+            );
+
+            return savedRide;
+        }
 
         pendingRidePool.addRide(savedRide);
 
--- pediuber/src/test/java/com/pediuber/pediuber/service/RideServiceTests.java
+++ pediuber/src/test/java/com/pediuber/pediuber/service/RideServiceTests.java
@@ -1,15 +1,27 @@
 package com.pediuber.pediuber.service;
 
+import com.pediuber.pediuber.dto.RideQueueMessage;
+import com.pediuber.pediuber.entity.Passenger;
 import com.pediuber.pediuber.entity.Ride;
 import com.pediuber.pediuber.enums.RideStatus;
 import com.pediuber.pediuber.logging.LoggingService;
+import com.pediuber.pediuber.policy.OverflowPolicyService;
 import com.pediuber.pediuber.pool.PendingRidePool;
+import com.pediuber.pediuber.rabbitmq.RideProducer;
 import com.pediuber.pediuber.repository.DriverRepository;
 import com.pediuber.pediuber.repository.PassengerRepository;
 import com.pediuber.pediuber.repository.RideRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class RideServiceTests {
 
@@ -19,6 +31,8 @@ public class RideServiceTests {
     private PendingRidePool pendingRidePool;
     private LoggingService loggingService;
     private RideService rideService;
+    private OverflowPolicyService overflowPolicyService;
+    private RideProducer rideProducer;
 
     @BeforeEach
     void setup() {
@@ -33,12 +47,19 @@ public class RideServiceTests {
 
         loggingService = Mockito.mock(LoggingService.class);
 
+        overflowPolicyService = Mockito.mock(OverflowPolicyService.class);
+
+        rideProducer = Mockito.mock(RideProducer.class);
+
         rideService = new RideService(
                 rideRepository,
                 driverRepository,
                 passengerRepository,
                 pendingRidePool,
-                loggingService
+                loggingService,
+                overflowPolicyService,
+                rideProducer
+
         );
     }
 
@@ -46,24 +67,20 @@ public class RideServiceTests {
     void shouldUpdateRideStatusFromRequestedToMatched() {
 
         Ride ride = new Ride();
-
         ride.setStatus(RideStatus.REQUESTED);
 
-        Mockito.when(
-                rideRepository.findById(1L)
-        ).thenReturn(java.util.Optional.of(ride));
+        when(rideRepository.findById(1L))
+                .thenReturn(Optional.of(ride));
 
-        Mockito.when(
-                rideRepository.save(Mockito.any())
-        ).thenAnswer(i -> i.getArgument(0));
+        when(rideRepository.save(any()))
+                .thenAnswer(i -> i.getArgument(0));
 
-        Ride result =
-                rideService.updateRideStatus(
-                        1L,
-                        RideStatus.MATCHED
-                );
+        Ride result = rideService.updateRideStatus(
+                1L,
+                RideStatus.MATCHED
+        );
 
-        assert result.getStatus() == RideStatus.MATCHED;
+        assertEquals(RideStatus.MATCHED, result.getStatus());
     }
 
     @Test
@@ -73,7 +90,7 @@ public class RideServiceTests {
 
         ride.setStatus(RideStatus.REQUESTED);
 
-        Mockito.when(
+        when(
                 rideRepository.findById(1L)
         ).thenReturn(java.util.Optional.of(ride));
 
@@ -84,6 +101,83 @@ public class RideServiceTests {
                         RideStatus.COMPLETED
                 )
         );
+    }
+
+    @Test
+    void shouldCreateRideAndAddToPendingPoolWhenNotOverloaded() {
+
+        Long passengerId = 1L;
+
+        Passenger passenger = new Passenger();
+        passenger.setId(passengerId);
+
+        Ride ride = new Ride();
+        ride.setOrigin("UFV");
+        ride.setDestination("Centro");
+
+        Ride savedRide = new Ride();
+        savedRide.setId(10L);
+        savedRide.setOrigin("UFV");
+        savedRide.setDestination("Centro");
+        savedRide.setPassenger(passenger);
+        savedRide.setStatus(RideStatus.REQUESTED);
+        savedRide.setCreatedAt(LocalDateTime.now());
+
+        when(passengerRepository.findById(passengerId))
+                .thenReturn(Optional.of(passenger));
+
+        when(rideRepository.save(any(Ride.class)))
+                .thenReturn(savedRide);
+
+        when(overflowPolicyService.isOverloaded())
+                .thenReturn(false);
+
+        Ride result = rideService.createRide(passengerId, ride);
+
+        assertEquals(RideStatus.REQUESTED, result.getStatus());
+        assertEquals(passenger, result.getPassenger());
+
+        verify(rideRepository).save(any(Ride.class));
+        verify(pendingRidePool).addRide(savedRide);
+        verify(rideProducer, never()).sendRideToOutputQueue(any());
+    }
+
+    @Test
+    void shouldSendRideToOutputQueueWhenOverloaded() {
+
+        Long passengerId = 1L;
+
+        Passenger passenger = new Passenger();
+        passenger.setId(passengerId);
+
+        Ride ride = new Ride();
+        ride.setOrigin("UFV");
+        ride.setDestination("Centro");
+
+        Ride savedRide = new Ride();
+        savedRide.setId(10L);
+        savedRide.setOrigin("UFV");
+        savedRide.setDestination("Centro");
+        savedRide.setPassenger(passenger);
+        savedRide.setStatus(RideStatus.REQUESTED);
+        savedRide.setCreatedAt(LocalDateTime.now());
+
+        when(passengerRepository.findById(passengerId))
+                .thenReturn(Optional.of(passenger));
+
+        when(rideRepository.save(any(Ride.class)))
+                .thenReturn(savedRide);
+
+        when(overflowPolicyService.isOverloaded())
+                .thenReturn(true);
+
+        Ride result = rideService.createRide(passengerId, ride);
+
+        assertEquals(RideStatus.REQUESTED, result.getStatus());
+
+        verify(rideRepository).save(any(Ride.class));
+        verify(rideProducer).sendRideToOutputQueue(any(RideQueueMessage.class));
+        verify(pendingRidePool, never()).addRide(any(Ride.class));
     }
 
 }
